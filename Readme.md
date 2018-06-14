ea89633d6b65f59b1f1216e53f2d953868c206cb
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-06-14 23:31:47 +0300

    Updated readme.

--- Readme.md
+++ Readme.md
@@ -29,8 +29,11 @@ This library implements Server peer for Networked A-Frame peer to peer mesh
 ### Commands
 
 ---
+    git clone https://github.com/tlaukkan/naf-mesh-server-peer.git
+    cd naf-mesh-server-peer
     heroku create <your-heroku-account>-naf-mesh-server-peer
     heroku config:set EMAIL=<enter your email> SECRET=<enter your secret>
+    heroku config:set SERVER_PEER_URLS=wss://tlaukkan-webrtc-signaling.herokuapp.com/31980bbf28e4b66e72ab49bebeb20da4f67a090c514d56c549f26caaf65a076c
     git push heroku master
     heroku logs -t
 ---
--- test/mesh-adapter-test.js
+++ test/mesh-adapter-test.js
@@ -154,7 +154,7 @@ describe('mesh-adapter', function() {
     })
 
     it('should connect to remote server peer and transmit message', function(done) {
-        this.timeout(5000);
+        this.timeout(8000);
         const adapter1 = new MeshAdapter(webrtc.RTCPeerConnection);
         adapter1.email = 'adapter1'
         adapter1.secret = 'adapter1'
@@ -162,25 +162,25 @@ describe('mesh-adapter', function() {
         adapter1.connect()
 
         adapter1.setServerConnectListeners((id) => {
-            console.log('adapter 1 connected to server and got id: ' + id)
+            console.log('adapter connected to server and got id: ' + id)
         }, () => {
-            console.log('adapter 1 server connect failed')
+            console.log('adapter server connect failed')
         })
 
         adapter1.setRoomOccupantListener((occupantMap) => {
-            console.log('adapter 1 occupant change')
+            console.log('adapter occupant change: ' + JSON.stringify(occupantMap))
             setTimeout(() => {
                 adapter1.disconnect()
                 done()
-            }, 2000)
+            }, 4000)
         })
 
         adapter1.setDataChannelListeners((id) => {
-                console.log('adapter 1 data channel opened from: ' + id)
+                console.log('adapter data channel opened from: ' + id)
             }, (id) => {
-                console.log('adapter 1 data channel closed from: ' + id)
+                console.log('adapter data channel closed from: ' + id)
             }, (id, dataType, data) => {
-                console.log('adapter 1 data channel message from: ' + id + ' ' + dataType + ' ' +data)
+                console.log('adapter data channel message from: ' + id + ' ' + dataType + ' ' +data)
             }
         )
 
